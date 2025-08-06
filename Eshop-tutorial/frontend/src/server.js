541e153722a7b23897235e5408ee57c5f81594be
Tahreem Rasheed <zarmeenjameel786@gmail.com>
2025-08-06 20:07:36 +0500

    Update server.js

--- Eshop-tutorial/frontend/src/server.js
+++ Eshop-tutorial/frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://multivendor-final.onrender.com/api/v2";
 
-export const backend_url="http://localhost:8000/"
+export const backend_url="https://multivendor-final.onrender.com/"
 
 
